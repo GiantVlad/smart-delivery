9f82f0ad8546b5728f3c42fff31d2445f573c0f6
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2024-11-15 16:12:01 +0300

    Task creation fix 1

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 use App\Dto\CreateOrderDto;
 use App\Enums\CourierStatusEnum;
 use App\Enums\OrderStatusEnum;
+use App\Enums\TaskStatusEnum;
 use App\Models\Courier;
 use App\Models\Customer;
 use App\Models\Order;
@@ -14,6 +15,8 @@ use App\Models\Point;
 use App\Models\Task;
 use App\Temporal\CreateOrderWorkflowInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Temporal\Client\WorkflowOptions;
 use Carbon\CarbonInterval;
 
@@ -47,15 +50,20 @@ class TaskController extends Controller
 //            CreateOrderWorkflowInterface::class,
 //            WorkflowOptions::new()->withWorkflowExecutionTimeout(CarbonInterval::minute())
 //        );
-
-        $customer = Courier::where('uuid', $courierUuid)->firstOrFail();
+        $courier = Courier::where('uuid', $courierUuid)->firstOrFail();
         $orders = Order::whereIn('id', $ordersIds)->get();
-        $task = new Task();
-        foreach ($orders as $order) {
-            $order->task()->associate($task);
-            $order->save();
-        }
-        $task->save();
+
+        DB::transaction(static function () use ($orders, $courier) {
+            $task = new Task();
+            foreach ($orders as $order) {
+                $order->task()->associate($task);
+                $order->save();
+            }
+            $task->courier()->associate($courier);
+            $task->uuid = Str::uuid()->toString();
+            $task->status = TaskStatusEnum::CREATED->value;
+            $task->save();
+        });
 
        //  $this->workflowClient->start($workflow, $orderDTO);
 
