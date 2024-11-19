f6508d0a6ae8d0c478c9bb2460f6e0f746e62925
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2024-11-19 10:08:20 +0300

    Route creation workflow

--- src/app/Http/Resources/OrderResource.php
+++ src/app/Http/Resources/OrderResource.php
@@ -22,7 +22,7 @@ class OrderResource extends JsonResource
             'unitType' => $this->unit_type,
             'status' => $this->status,
             'customerEmail' => $this->whenLoaded('customer') ? $this->customer->email : '',
-            'taskCourierName' => (new TaskResource($this->whenLoaded('task')))?->courierName ?? 'Undefined',
+            'taskCourierName' => $this->task->courier->name ?? 'Undefined',
             'startPointAddress' => $this->startPoint->address,
             'endPointAddress' => $this->endPoint->address,
             'startPointId' => $this->startPoint->id,
