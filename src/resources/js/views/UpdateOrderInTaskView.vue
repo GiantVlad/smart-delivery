d4ee7fb1854920fbe74226c2289bfa00e95d8f66
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2024-11-20 20:50:30 +0300

    assign order to a task

--- src/resources/js/views/UpdateOrderInTaskView.vue
+++ src/resources/js/views/UpdateOrderInTaskView.vue
@@ -206,7 +206,8 @@ const addOrdersToTask = () => {
           </tr>
           </tbody>
         </table>
-        <template #footer v-if="selectedTask">
+        <div v-if="selectedTask !== null">
+        <template #footer>
           <div v-if="orderSelector">
           <FormField label="Orders">
             <multiselect
@@ -237,6 +238,7 @@ const addOrdersToTask = () => {
             <BaseButton type="button" color="info" label="Add orders" @click="showOrderSelector"/>
           </BaseButtons>
         </template>
+        </div>
       </CardBox>
     </SectionMain>
 </LayoutAuthenticated>
