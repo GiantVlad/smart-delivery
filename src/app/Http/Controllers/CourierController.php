e2926fecc501b78813f5f16e0ebb9a0a808fd256
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2024-11-21 20:41:22 +0300

    Couriers page

--- src/app/Http/Controllers/CourierController.php
+++ src/app/Http/Controllers/CourierController.php
@@ -4,24 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Dto\AssignOrderDto;
-use App\Enums\OrderStatusEnum;
-use App\Http\Requests\AddOrderRequest;
-use App\Http\Requests\UnassignOrderRequest;
 use App\Http\Resources\CourierResource;
-use App\Http\Resources\OrderResource;
-use App\Http\Resources\OrderToAssignResource;
 use App\Models\Courier;
-use App\Models\Order;
-use App\Models\Task;
-use App\Temporal\AssignOrderWorkflowInterface;
-use App\Temporal\UnassignOrderWorkflowInterface;
-use Carbon\CarbonInterval;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Temporal\Client\WorkflowOptions;
-
-use function PHPUnit\Framework\isEmpty;
 
 class CourierController extends Controller
 {
