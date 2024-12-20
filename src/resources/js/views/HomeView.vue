c305d121763d04dec805b652139155eb19c0ab59
Uladzimir Sadkou <uladzimir.sadkou@paxful.com>
2024-12-20 16:24:42 +0300

    google map

--- src/resources/js/views/HomeView.vue
+++ src/resources/js/views/HomeView.vue
@@ -13,6 +13,7 @@ import LineChart from '@/components/Charts/LineChart.vue'
 import SectionMain from '@/components/SectionMain.vue'
 import CardBoxWidget from '@/components/CardBoxWidget.vue'
 import CardBox from '@/components/CardBox.vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
 import NotificationBar from '@/components/NotificationBar.vue'
 import BaseButton from '@/components/BaseButton.vue'
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
@@ -29,18 +30,21 @@ onMounted(() => {
   fillChartData()
 })
 
+const center = { lat: 40.689247, lng: -74.044502 }
+
 </script>
 
 <template>
   <LayoutAuthenticated>
     <SectionMain>
-      <h3>My Google Maps Demo</h3>
-      <!--The div element for the map -->
-      <div id="map"></div>
-
-      <!-- prettier-ignore -->
-      <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-      ({key: "AIzaSyDF6ZTPdgnpG9wp_gRRWWWfI-QLI0pw1Tg", v: "weekly"});</script>
+      <GoogleMap
+        api-key="AIzaSyDF6ZTPdgnpG9wp_gRRWWWfI-QLI0pw1Tg"
+        style="width: 100%; height: 500px"
+        :center="center"
+        :zoom="15"
+      >
+        <Marker :options="{ position: center }" />
+      </GoogleMap>
       <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
         <CardBoxWidget
           trend="12%"
