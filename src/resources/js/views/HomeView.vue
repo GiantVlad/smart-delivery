4c4fe34bfa4356e6fbe85877878417e7d6bd59e8
Uladzimir Sadkou <uladzimir.sadkou@paxful.com>
2024-12-20 15:57:31 +0300

    google map

--- src/resources/js/views/HomeView.vue
+++ src/resources/js/views/HomeView.vue
@@ -34,6 +34,13 @@ onMounted(() => {
 <template>
   <LayoutAuthenticated>
     <SectionMain>
+      <h3>My Google Maps Demo</h3>
+      <!--The div element for the map -->
+      <div id="map"></div>
+
+      <!-- prettier-ignore -->
+      <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+      ({key: "AIzaSyDF6ZTPdgnpG9wp_gRRWWWfI-QLI0pw1Tg", v: "weekly"});</script>
       <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
         <CardBoxWidget
           trend="12%"
