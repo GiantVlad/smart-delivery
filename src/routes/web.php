6b45ef5579fb88daabc4509b4f547d09353d5d56
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2026-05-25 23:09:12 +0300

    github ci-cd fix 12

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
-
 Route::get('/login', [DashboardController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/auth', [DashboardController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/auth', [DashboardController::class, 'index']);
+});
