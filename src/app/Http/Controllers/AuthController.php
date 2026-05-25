8e3695390dbfa35d26ac8680f9d4439c23d7ca56
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2026-05-25 16:11:51 +0300

    github ci-cd fix 4

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -17,7 +17,9 @@ class AuthController extends Controller
         $remember = $request->boolean('remember');
 
         if (Auth::attempt($credentials, $remember)) {
-            $request->session()->regenerate();
+            if ($request->hasSession()) {
+                $request->session()->regenerate();
+            }
 
             return response()->json([
                 'data' => [
@@ -40,8 +42,10 @@ class AuthController extends Controller
 
             // Clear session data
             auth()->logout();
-            request()->session()->invalidate();
-            request()->session()->regenerateToken();
+            if (request()->hasSession()) {
+                request()->session()->invalidate();
+                request()->session()->regenerateToken();
+            }
 
             return response()->json([
                 'message' => 'Successfully logged out',
