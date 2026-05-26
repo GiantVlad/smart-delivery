cb9e327298a8619ed130ee578ad09409219a79ba
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2026-05-26 06:52:29 +0300

    fix login route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [DashboardController::class, 'index']);
+Route::get('/login', [DashboardController::class, 'index'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
