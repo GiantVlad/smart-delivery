9fd8838aac346422fe03cf64d1f69ed39fadc942
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2026-05-30 09:50:31 +0300

    fixed centrifugo

--- src/app/Console/Commands/TestCentrifugoPush.php
+++ src/app/Console/Commands/TestCentrifugoPush.php
@@ -36,8 +36,9 @@ class TestCentrifugoPush extends Command
         $this->info('Subscription token '.substr($apiSign, 0, 5).'...');
 
         // Get a list of currently active channels.
-        $channels = $centrifugo->channels() ?? [];
-        $this->info('Active channels '.implode(',', $channels));
+        $channelsResponse = $centrifugo->channels() ?? [];
+        $channels = array_keys((array) ($channelsResponse['channels'] ?? []));
+        $this->info('Active channels '.(empty($channels) ? 'none' : implode(',', $channels)));
 
         // Get channel presence information (all clients currently subscribed on this channel).
         $centrifugo->presence('test');
