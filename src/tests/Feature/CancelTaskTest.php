f985bc01f2a3f1de50230c31a03cf1d5eecf39ec
ul.sadkou <uladzimir.sadkou@xpgraph.com>
2026-08-07 11:32:42 +0300

    temporal as source of true - refactoring. Fixed tests

--- src/tests/Feature/CancelTaskTest.php
+++ src/tests/Feature/CancelTaskTest.php
@@ -101,6 +101,9 @@ class CancelTaskTest extends TestCase
         $taskWorkflowMock->shouldReceive('cancel')
             ->once()
             ->andReturnNull();
+        $taskWorkflowMock->shouldReceive('getState')
+            ->once()
+            ->andReturnNull();
 
         $this->mock(WorkflowClientInterface::class, function (MockInterface $mock) use ($taskWorkflowMock): void {
             $mock->shouldReceive('newRunningWorkflowStub')
@@ -136,6 +139,9 @@ class CancelTaskTest extends TestCase
             ->andThrow(WorkflowNotFoundException::withoutMessage(
                 new WorkflowExecution('task:'.$this->task->uuid),
             ));
+        $taskWorkflowMock->shouldReceive('getState')
+            ->once()
+            ->andReturnNull();
 
         // Mock order workflow being found and signaled
         $orderWorkflowMock = Mockery::mock(OrderWorkflowInterface::class);
@@ -188,6 +194,9 @@ class CancelTaskTest extends TestCase
             ->andThrow(WorkflowNotFoundException::withoutMessage(
                 new WorkflowExecution('task:'.$this->task->uuid),
             ));
+        $taskWorkflowMock->shouldReceive('getState')
+            ->once()
+            ->andReturnNull();
 
         // Mock order workflow stub whose unassignFromTask() throws WorkflowNotFoundException
         $orderWorkflowMock = Mockery::mock(OrderWorkflowInterface::class);
